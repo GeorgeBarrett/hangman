466221006c2e52c29602496c0be858687e42e71d
George Barrett <georgeaustinbarrett@gmail.com>
2022-11-01 14:19:41 +0000

    app ready to run

--- hangman.py
+++ hangman.py
@@ -15,17 +15,25 @@ def hangman():
     alphabet = set(string.ascii_uppercase)
     used_letters = set() # what the user has guessed
 
-    user_letter = input('Guess a letter:').uppercase() # getting user input
-    if user_input in alphabet - used_letters:
-        used_letters.add(user_letter)
-        if user_letter in word_letters:
-            word_letters.remove(user_letter)
-    
-    elif user_letter in used_letters:
-        print('You have already used this letter')
-    
-    else:
-        print('Invalid character')
+    while len(word_letters) > 0:
+
+        print('You have already used the letters:', ''.join(used_letters)) # letters used
+
+        word_list = [letter if letter in used_letters else '' for letter in word] # location of the current word
+        print('Current word: ', ''.join(word_list))
+
+
+        user_letter = input('Guess a letter:').uppercase() # getting user input
+        if user_input in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+        
+        elif user_letter in used_letters:
+            print('You have already used this letter')
+        
+        else:
+            print('Invalid character')
 
 user_input = input('Type something:')
 print(user_input)
